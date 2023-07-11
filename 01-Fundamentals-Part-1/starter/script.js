1a14880c05355f72f646b3395ba095c53fe7c2db
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2023-07-11 13:31:05 -0300

    terminei modulo basico 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -130,6 +130,58 @@ if (BMIJohn > BMIMark) {
     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
 }
 
-*/
- 
 
+
+// BOOLEAN LOGIC
+ 
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision
+
+if (shouldDrive) {
+    console.log(`Is able to drive!`)
+} else {
+    console.log(`Someone else should drive...`)
+}
+
+
+const day = `thursday`;
+
+switch(day){
+
+    case `monday`:  // day == `monday`
+        console.log(`Plan course structure`);
+        console.log(`Go to coding meetup`);
+        break;
+
+    case `tuesday`:
+        console.log(`Prepare theory videos`);
+        break;
+
+    case `wednesday`:
+    case `thursday`:
+        console.log(`Write code examples`);
+        break;
+
+    case `friday`:
+        console.log(`Record videos`);
+        break;
+
+    case `saturday`:
+    case `sunday`:
+        console.log(`Enjoy the weekend`)
+        break;
+    default:
+        console.log(`Not a valid day!`)
+}
+
+const age = 23;
+age >= 18 ? console.log(`I like to drink wine`):
+console.log(`I like to drink water`)
+
+*/
