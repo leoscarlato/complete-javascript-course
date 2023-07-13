4c64dddedd7019c646fdae747f14e0732992eae0
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2023-07-13 18:02:56 -0300

    acabei mais uma seção

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    console.log(`${array[i]}°C in ${i + 1} days.`)
+  }
+};
+
+printForecast(data1);
