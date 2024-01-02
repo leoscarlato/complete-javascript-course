10ce80c629ab3a287fe2c1e9aac80ded22721715
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-01-02 16:13:48 -0300

    destructuring arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,34 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order : function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  }
 };
+
+// Destructuring Arrays
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2]; // old way
+
+const [x, y, z] = arr; // destructuring way
+console.log(x, y, z);
+
+let [main, , secondary] = restaurant.categories; // skip second element
+console.log(main, secondary);
+
+// Switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+// destructuring way
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
