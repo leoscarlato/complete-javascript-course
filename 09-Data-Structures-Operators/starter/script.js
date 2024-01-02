6ca53456207d6e8a802dc5f11efcdd014f75fd5d
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-01-02 17:50:54 -0300

    logical assignment operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,15 +54,43 @@ const restaurant = {
   },
 };
 
-restaurant.numGuests = 0;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+const rest1 = {
+  name: 'Capri',
+  numGuests: 0,
+};
 
-// Nullish: null and undefined (does not include 0 or '' (0 and '' are not falsy values in this case))
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+const rest2 = {
+  name: 'La Piazza',
+  owner: `Giovanni Rossi`,
+};
 
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
 
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// // AND assignment operator
+// rest1.owner = rest1.owner && `<ANONYMOUS>`;
+// rest2.owner = rest2.owner && `<ANONYMOUS>`;
+rest1.owner &&= `<ANONYMOUS>`;
+rest2.owner &&= `<ANONYMOUS>`;
+
+console.log(rest1);
+console.log(rest2);
+
+// restaurant.numGuests = 0;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+
+// // Nullish: null and undefined (does not include 0 or '' (0 and '' are not falsy values in this case))
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
 
 // console.log(`--------- OR --------`);
 
@@ -73,7 +101,6 @@ console.log(guestCorrect);
 // console.log(undefined || null);
 
 // console.log(undefined || 0 || '' || 'Hello' || 23 || null); // retorna Hello -> primeiro valor que é truthful
-
 
 // const guests2 = restaurant.numGuests || 10;
 // console.log(guests2);
@@ -228,3 +255,22 @@ console.log(guestCorrect);
 // // Receive 2 return values from a function
 // const [starter, mainCourse] = restaurant.order(2, 0);
 // console.log(starter, mainCourse);
+
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+*/
+
