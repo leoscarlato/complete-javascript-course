bb51e2c2505306f924b438ac065f15aa9d6e00a1
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-01-15 18:32:14 -0300

    string pt1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -643,38 +643,72 @@ and the keys are the minutes in which each event happened (a football game has 9
 GOOD LUCK 😀
 */
 
-const gameEvents = new Map([
-  [17, '⚽️ GOAL'],
-  [36, '🔁 Substitution'],
-  [47, '⚽️ GOAL'],
-  [61, '🔁 Substitution'],
-  [64, '🔶 Yellow card'],
-  [69, '🔴 Red card'],
-  [70, '🔁 Substitution'],
-  [72, '🔁 Substitution'],
-  [76, '⚽️ GOAL'],
-  [80, '⚽️ GOAL'],
-  [92, '🔶 Yellow card'],
-]);
+// const gameEvents = new Map([
+//   [17, '⚽️ GOAL'],
+//   [36, '🔁 Substitution'],
+//   [47, '⚽️ GOAL'],
+//   [61, '🔁 Substitution'],
+//   [64, '🔶 Yellow card'],
+//   [69, '🔴 Red card'],
+//   [70, '🔁 Substitution'],
+//   [72, '🔁 Substitution'],
+//   [76, '⚽️ GOAL'],
+//   [80, '⚽️ GOAL'],
+//   [92, '🔶 Yellow card'],
+// ]);
 
-// 1.
-const events = [...new Set(gameEvents.values())];
-console.log(events);
+// // 1.
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
 
-// 2.
-gameEvents.delete(64);
-console.log(gameEvents);
+// // 2.
+// gameEvents.delete(64);
+// console.log(gameEvents);
 
-// 3.
-console.log(
-  `An event happened, on average, every ${90 / gameEvents.size} minutes`
-);
+// // 3.
+// console.log(
+//   `An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
 
-// 4.
-for (const [key, value] of gameEvents) {
-  if (key < 45) {
-    console.log(`[FIRST HALF] ${key}: ${value}`);
+// // 4.
+// for (const [key, value] of gameEvents) {
+//   if (key < 45) {
+//     console.log(`[FIRST HALF] ${key}: ${value}`);
+//   } else {
+//     console.log(`[SECOND HALF] ${key}: ${value}`);
+//   }
+// }
+
+// Strings
+const airline = `LATAM`;
+const plane = `A320`;
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(`B737`[0]);
+
+console.log(airline.length);
+console.log(`B737`.length);
+
+console.log(airline.indexOf(`L`));
+console.log(airline.lastIndexOf(`A`));
+
+console.log(airline.slice(2));
+console.log(airline.slice(2, 4));
+
+console.log(airline.slice(0, airline.indexOf(`T`)));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === `B` || s === `E`) {
+    console.log(`You got the middle seat`);
   } else {
-    console.log(`[SECOND HALF] ${key}: ${value}`);
+    console.log(`You got lucky`);
   }
-}
+};
+
+checkMiddleSeat(`11B`);
+checkMiddleSeat(`23C`);
+checkMiddleSeat(`3E`);
