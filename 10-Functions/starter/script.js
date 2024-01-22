98f665cd7cfb6d52c548298ebc4ed8dc5aee5be8
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-01-22 17:39:36 -0300

    functions accepting callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -21,31 +21,56 @@
 // createBooking(`LH123`, 2);
 // createBooking(`LH123`, undefined, 1000);
 
-const flight = `LH234`;
-const leo = {
-  name: `Leonardo Scarlato`,
-  passport: 230872340892341,
-};
+// const flight = `LH234`;
+// const leo = {
+//   name: `Leonardo Scarlato`,
+//   passport: 230872340892341,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = `LH999`;
-  passenger.name = `Mr. ` + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = `LH999`;
+//   passenger.name = `Mr. ` + passenger.name;
 
-  if (passenger.passport === 230872340892341) {
-    alert(`Check In`);
-  } else {
-    alert(`Wrong Passport`);
-  }
-};
+//   if (passenger.passport === 230872340892341) {
+//     alert(`Check In`);
+//   } else {
+//     alert(`Wrong Passport`);
+//   }
+// };
 
+// // checkIn(flight, leo);
+// // console.log(flight);
+// // console.log(leo);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000000);
+// };
+
+// newPassport(leo);
 // checkIn(flight, leo);
-// console.log(flight);
 // console.log(leo);
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000000);
+const oneWord = function (str) {
+  return str.replace(/ /g, ``).toLowerCase();
 };
 
-newPassport(leo);
-checkIn(flight, leo);
-console.log(leo);
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(` `);
+  return [first.toUpperCase(), ...others].join(` `);
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer(`JavaScript is the best!`, upperFirstWord);
+transformer(`JavaScript is the best!`, oneWord);
+
+const high5 = function () {
+  console.log(`🙌`);
+};
+
+document.body.addEventListener(`click`, high5);
