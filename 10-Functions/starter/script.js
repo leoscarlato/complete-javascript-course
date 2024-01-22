18d0f723e3fa032e0634cb1390e8510cadec997b
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-01-22 17:45:18 -0300

    functions returning functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -50,27 +50,42 @@
 // checkIn(flight, leo);
 // console.log(leo);
 
-const oneWord = function (str) {
-  return str.replace(/ /g, ``).toLowerCase();
+// const oneWord = function (str) {
+//   return str.replace(/ /g, ``).toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(` `);
+//   return [first.toUpperCase(), ...others].join(` `);
+// };
+
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer(`JavaScript is the best!`, upperFirstWord);
+// transformer(`JavaScript is the best!`, oneWord);
+
+// const high5 = function () {
+//   console.log(`🙌`);
+// };
+
+// document.body.addEventListener(`click`, high5);
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
 };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(` `);
-  return [first.toUpperCase(), ...others].join(` `);
-};
+const greeterHey = greet(`Hey`);
+greeterHey(`Leo`);
+greeterHey(`Joao`);
 
-// Higher-order function
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
-  console.log(`Transformed by: ${fn.name}`);
-};
+greet(`Hello`)(`Leo`);
 
-transformer(`JavaScript is the best!`, upperFirstWord);
-transformer(`JavaScript is the best!`, oneWord);
-
-const high5 = function () {
-  console.log(`🙌`);
-};
-
-document.body.addEventListener(`click`, high5);
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+greetArr(`Hello`)(`Leo`);
