7076f9f48ba08b2ea9798f2a237744f5bacc1609
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-01-26 17:42:20 -0300

    closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -214,38 +214,92 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
 */
 
-const poll = {
-  question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-  // This generates [0, 0, 0, 0]. More in the next section 😃
-  answers: new Array(4).fill(0),
-  registerNewAnswer: function () {
-    const answer = Number(
-      prompt(
-        `${this.question}\n${this.options.join(`\n`)}\n(Write option number)`
-      )
-    );
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section 😃
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer: function () {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join(`\n`)}\n(Write option number)`
+//       )
+//     );
 
-    typeof answer === 'number' &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
+//     typeof answer === 'number' &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
 
-    this.displayResults();
-    this.displayResults(`string`);
-  },
+//     this.displayResults();
+//     this.displayResults(`string`);
+//   },
 
-  displayResults(type = `array`) {
-    if (type == `array`) {
-      console.log(this.answers);
-    } else if (type == `string`) {
-      console.log(`Poll results arre ${this.answers.join(`, `)}`);
-    }
-  },
+//   displayResults(type = `array`) {
+//     if (type == `array`) {
+//       console.log(this.answers);
+//     } else if (type == `string`) {
+//       console.log(`Poll results arre ${this.answers.join(`, `)}`);
+//     }
+//   },
+// };
+
+// // poll.registerNewAnswer();
+// document
+//   .querySelector(`.poll`)
+//   .addEventListener(`click`, poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({ answers: [5, 2, 3] }, `string`);
+
+////// Closures
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
 };
 
-// poll.registerNewAnswer();
-document
-  .querySelector(`.poll`)
-  .addEventListener(`click`, poll.registerNewAnswer.bind(poll));
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
 
-poll.displayResults.call({ answers: [5, 2, 3] }, `string`);
+g();
+f();
+
+// Re-assigning f function
+h();
+f();
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
