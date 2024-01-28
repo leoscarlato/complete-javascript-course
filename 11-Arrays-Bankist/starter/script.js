b371a5287ceea2d80f78311114c089c8b72c2646
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-01-28 18:19:05 -0300

    coding challenge #1 + start bankist app

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,11 +61,37 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = ``;
+
+  var sum = 0;
+
+  for (let i = 0; i < movements.length; i++) {
+    sum += movements[i];
+  }
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? `deposit` : `withdrawal`;
+
+    const html = `<div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__date">3 days ago</div>
+    <div class="movements__value">${mov}</div>
+  </div>`;
+
+    containerMovements.insertAdjacentHTML(`afterbegin`, html);
+  });
+
+  labelBalance.textContent = `${sum} EUR`;
+};
+
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -125,19 +151,67 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // });
 
 // Map
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-currencies.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`);
-});
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
 
-// Set
-const currenciesUnique = new Set([`USD`, `GBP`, `USD`, `EUR`, `EUR`]);
-console.log(currenciesUnique);
-currenciesUnique.forEach(function (value, _, map) {
-  console.log(`${value}: ${value}`);
-});
+// // Set
+// const currenciesUnique = new Set([`USD`, `GBP`, `USD`, `EUR`, `EUR`]);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, _, map) {
+//   console.log(`${value}: ${value}`);
+// });
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, 
+and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, 
+and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! 
+So create a shallow copy of Julia's array, and remove the cat 
+ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  // 1 - Remove first and last two from julia`s array
+  const newJulia = dogsJulia.slice(1, -2);
+
+  // 2 - New array with both Julia and Kate dogs
+  const allDogs = [...newJulia, ...dogsKate];
+
+  // 3 - Determine if the dogs are adults or not
+  allDogs.forEach(function (age, i) {
+    if (age >= 3) {
+      console.log(`Dog ${i} is an adult, and is ${age} years old`);
+    } else {
+      console.log(`Dog number ${i} is still a puppy 🐶`);
+    }
+  });
+};
+
+console.log(`------------------ PRIMEIRA ENTRADA -----------------`);
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+console.log(`------------------ SEGUNDA ENTRADA -----------------`);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
