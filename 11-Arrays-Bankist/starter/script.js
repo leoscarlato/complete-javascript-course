16d528e6e8b50b072f6ff753a6d650e94e02b011
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-01-28 18:50:42 -0300

    reduce bankist

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -64,12 +64,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 const displayMovements = function (movements) {
   containerMovements.innerHTML = ``;
 
-  var sum = 0;
-
-  for (let i = 0; i < movements.length; i++) {
-    sum += movements[i];
-  }
-
   movements.forEach(function (mov, i) {
     const type = mov > 0 ? `deposit` : `withdrawal`;
 
@@ -81,8 +75,6 @@ const displayMovements = function (movements) {
 
     containerMovements.insertAdjacentHTML(`afterbegin`, html);
   });
-
-  labelBalance.textContent = `${sum} EUR`;
 };
 
 displayMovements(account1.movements);
@@ -99,6 +91,13 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 // console.log(accounts);
+
+const calcPrintBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = balance
+};
+
+calcPrintBalance(account1.movements);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -256,13 +255,36 @@ GOOD LUCK 😀
 
 ///////////////////////////////////////
 
-const deposits = movements.filter(function (mov) {
-  return mov > 0;
-});
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
 
-console.log(movements);
-console.log(deposits);
+// console.log(movements);
+// console.log(deposits);
 
-const withdrawals = movements.filter(mov => mov < 0);
+// const withdrawals = movements.filter(mov => mov < 0);
 
-console.log(withdrawals);
+// console.log(withdrawals);
+
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   return acc + cur;
+// }, 0);
+// console.log(balance);
+
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) {
+    return acc;
+  } else {
+    return mov;
+  }
+}, movements[0]);
+
+console.log(max);
